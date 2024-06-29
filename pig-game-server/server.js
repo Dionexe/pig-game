c46701803dccc0952960d7dfd9319a8a6de9e266
Dion Wells <dionwells@Dions-MacBook-Pro.local>
2024-06-29 11:51:13 -0700

    Fixed Fonts

--- pig-game-server/server.js
+++ pig-game-server/server.js
@@ -8,17 +8,19 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader("Content-Security-Policy", "default-src 'self'; style-src 'self' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com");
+    next();
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 const pool = new Pool({
     user: process.env.PG_USER,
     host: process.env.PG_HOST,
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
     port: process.env.PG_PORT,
-});
-
-app.use((req, res, next) => {
-    res.setHeader("Content-Security-Policy", "default-src 'self'; style-src 'self' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com");
-    next();
 });
 
 app.get('/high-scores', async (req, res) => {
